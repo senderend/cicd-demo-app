fbf7864e42907476489c9b9794a411dd68cd9d4e
Allen Demoura <me@allendemoura.com>
2026-07-15 17:25:12 -0700

    nosemgrep

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def get_item(item_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # nosemgrep: python.flask.security.audit.app-run-param-config.avoid_app_run_with_bad_host
+    app.run(host="0.0.0.0", port=5000) 
